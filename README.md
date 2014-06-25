900fda408476efb69597345d664a47aad18ca548
greganki <KieranGregan@Kierans-MacBook-Pro.local>
2014-06-26 09:16:50 +1200

    Adding the code etc.

---
+++ README.md
@@ -0,0 +1,7 @@
+Pizza
+=====
+
+A sample pizza ordering bootstrap site
+
+[![Screenshot](https://raw.githubusercontent.com/redhoodie/pizza/master/screenshot.png)](#Screenshot)
+[![Screenshot2](https://raw.githubusercontent.com/redhoodie/pizza/master/screenshot_orders.png)](#Screenshot2)
---
+++ htdocs/js/scripts.js
@@ -0,0 +1,80 @@
+// The order is stored here
+var order = [];
+var max_pizzas = 5;
+var orders = load_orders();
+var delivery_price = 3.00;
+
+var pizzas = [
+  {name: 'Supermisso', price: 8.50, image: 'pizza-1.png'},
+  {name: 'Meatlovers', price: 8.50, image: 'pizza-2.png'},
+  {name: 'Supreme', price: 8.50, image: 'pizza-3.png'},
+  {name: 'Hawaiian', price: 8.50, image: 'pizza-4.png'},
+  {name: 'Simply Cheese', price: 8.50, image: 'pizza-1.png'},
+  {name: 'Pepperoni', price: 8.50, image: 'pizza-2.png'},
+  {name: 'Margherita', price: 8.50, image: 'pizza-3.png'},
+  {name: 'Garlic Prawn', price: 13.50, image: 'pizza-4.png'},
+  {name: 'Vegorama', price: 13.50, image: 'pizza-1.png'},
+  {name: 'Veg Trio', price: 13.50, image: 'pizza-2.png'},
+  {name: 'Funky Chicken', price: 13.50, image: 'pizza-3.png'},
+  {name: 'Apricot Chicken', price: 13.50, image: 'pizza-4.png'},
+];
+
+// Try and load orders from localStorage
+function load_orders(){
+  try {
+    return JSON.parse(window.localStorage["orders"]);
+  }
+  catch (e) {
+    return [];
+  }
+}
+
+function save_order() {
+  orders.push({pizzas: order, form: $('form').serializeArray()});
+
+  save_orders();
+}
+
+function save_orders() {
+  localStorage.setItem('orders', JSON.stringify(orders));
+}
+
+function get_order_html(pizzas, delivery, show_delete) {
+    order_html = "";
+
+//Add text box when delivery is choosen
+    if (delivery) {
+      order_html += '<p>';
+      order_html += '<input type="text" class="form-control" id="address" name="address" placeholder="Address"><br/>';
+      order_html += '<input type="tel" class="form-control" id="phone" name="phone" placeholder="Phone Number"> ';
+      order_html += '</p>';
+    }
+
+    total = 0.00;
+
+    // Add Pizzas to the order
+    for (var i = 0; i < pizzas.length; ++i) {
+      pizza = pizzas[i];
+      total += pizza.price;
+
+      order_html += '<tr id="' + i + '"><th>' + pizza.name + '</th><td>$' + pizza.price.toFixed(2) + '</td>';
+      if (show_delete) {
+        order_html += '<td><button type="button" class="btn btn-default btn-xs"><span class="glyphicon glyphicon-remove"></span></button></td>';
+      }
+      order_html += '</tr>';
+    }
+
+    // Add Delivery to the order
+    if (delivery) {
+      total += delivery_price;
+      order_html += '<tr><th>Delivery</th><td>$' + delivery_price.toFixed(2) + '</td></tr>';
+    }
+
+    // Add GST
+    order_html += '<tr><th>GST</th><th>$' + (total * 3 / 23).toFixed(2) + '</th></tr>';
+
+    // Add price total
+    order_html += '<tr><th>Total</th><th>$' + total + '</th></tr>';
+
+    return order_html;
+}
